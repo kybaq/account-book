3487e411006a40161505daf02d77f81dfb0d47de
kybaq <flfsk12@gmail.com>
2024-05-28 20:06:29 +0900

    select 라이브러리 삭제, 예외처리 추가 및 삭제 기능 추가

--- src/pages/AccountDetail.jsx
+++ src/pages/AccountDetail.jsx
@@ -1,14 +1,6 @@
 import Swal from "sweetalert2";
-import Select from "react-select";
 import React, { useRef } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
-
-const options = [
-  { value: "식비", label: "식비" },
-  { value: "교통", label: "교통" },
-  { value: "취미/여가", label: "취미/여가" },
-  { value: "생활비", label: "생활비" },
-];
+import { Link, useLocation } from "react-router-dom";
 
 function AccountDetail() {
   // fillteredMonth 를 받아 id 비교를 해서 해당 expense 의 내용을 수정 / 삭제 가능하도록!
@@ -17,8 +9,6 @@ function AccountDetail() {
 
   const path = location.pathname.split("/");
   const id = path[path.length - 1];
-
-  console.log(id);
 
   // NOTE:지금은 데이터 크기가 작아서 전체 목록에서 찾아도 문제 없겠지만, 사실은 filteredExpense 에서 내용을 가져오도록 해야 약간이나마 효율적일듯
   const totalExpenses = JSON.parse(
@@ -31,9 +21,7 @@ function AccountDetail() {
   const billRef = useRef(null);
   const descriptiontRef = useRef(null);
 
-  const onHandleDetailFormSubmit = (evt) => {
-    evt.preventDefault();
-
+  const onHandleSaveBtn = (id) => {
     if (
       !dateRef.current ||
       !categoryRef.current ||
@@ -48,11 +36,20 @@ function AccountDetail() {
       });
       return;
     }
+    if (isNaN(bill) || bill <= 0) {
+      Swal.fire({
+        title: "오류",
+        text: "금액은 양수여야 합니다.",
+        icon: "warning",
+        confirmButtonText: "확인",
+      });
+      return;
+    }
 
     const nextExpense = {
       id,
       date: dateRef.current,
-      category: categoryRef.current.props.value.value, // 외부 라이브러리를 통해서 그런지 조금 복잡해졌다..
+      category: categoryRef.current,
       bill: billRef.current,
       description: descriptiontRef.current,
     };
@@ -61,43 +58,57 @@ function AccountDetail() {
       `수정: ${nextExpense.date} ${nextExpense.category} ${nextExpense.bill} ${nextExpense.description}`
     );
 
-    totalExpenses.forEach((expense) => {
-      if (expense.id === id) {
-        expense.date = nextExpense.date;
-        expense.category = nextExpense.category;
-        expense.bill = nextExpense.bill;
-        expense.description = nextExpense.description;
-      }
-    });
+    totalExpenses.map((expense) =>
+      expense.id === id ? { ...expense, ...nextExpense } : expense
+    );
+    // expense 객체를 펼쳐서, nextExpense 로 내용을 바꿈.
 
     // Ref 값으로 기존 값을 대체해서 localStorage 에 올림.
     window.localStorage.setItem("totalExpenses", JSON.stringify(totalExpenses));
+    window.history.back();
+  };
+
+  const onHandleDeleteBtn = (id) => {
+    const nextTotalExpenses = totalExpenses.filter(
+      (expense) => expense.id !== id
+    );
+    window.localStorage.setItem(
+      "totalExpenses",
+      JSON.stringify(nextTotalExpenses)
+    );
+    window.history.back();
   };
 
   return (
     <section>
-      <form action="" onSubmit={onHandleDetailFormSubmit}>
-        <input
-          type="date"
-          ref={dateRef}
-          onChange={(evt) => (dateRef.current = evt.target.value)}
-        />
-        <Select options={options} ref={categoryRef} />
-        <input
-          type="number"
-          placeholder="금액"
-          ref={billRef}
-          onChange={(evt) => (billRef.current = evt.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="지출 세부 내용"
-          ref={descriptiontRef}
-          onChange={(evt) => (descriptiontRef.current = evt.target.value)}
-        />
-        <button type="submit">저장</button>
-        <button type="submit">삭제</button>
-      </form>
+      <input
+        type="date"
+        ref={dateRef}
+        onChange={(evt) => (dateRef.current = evt.target.value)}
+        defaultValue={location.state.expense.date}
+      />
+      <select ref={categoryRef}>
+        <option value="식비">식비</option>
+        <option value="교통">교통</option>
+        <option value="취미/여가">취미/여가</option>
+        <option value="생활비">생활비</option>
+      </select>
+      <input
+        type="number"
+        placeholder="금액"
+        ref={billRef}
+        onChange={(evt) => (billRef.current = evt.target.value)}
+        defaultValue={location.state.expense.bill}
+      />
+      <input
+        type="text"
+        placeholder="지출 세부 내용"
+        ref={descriptiontRef}
+        onChange={(evt) => (descriptiontRef.current = evt.target.value)}
+        defaultValue={location.state.expense.description}
+      />
+      <button onClick={() => onHandleSaveBtn(id)}>저장</button>
+      <button onClick={() => onHandleDeleteBtn(id)}>삭제</button>
       <Link to="/">뒤로가기</Link>
       {/* history 써서 뒤로 돌아가면 될듯 */}
     </section>
